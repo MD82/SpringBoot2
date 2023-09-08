156e0792f9ca4a805be16a826ef2961afe4c57cb
kim.manho <kim.manho@joins.com>
2023-09-09 07:09:52 +0900

    [#][FEAT] yml 추가

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,9 +8,6 @@ import java.util.Collections;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port","8083"));
-        app.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
